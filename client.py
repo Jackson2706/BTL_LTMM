288500c887a56da86a619006e6010f45b1244fc5
Jackson2706 <dung27062002@gmail.com>
2023-05-17 14:27:06 +0700

    first commit

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = "127.0.0.1"
+SERVER_PORT = 65432
+FORMAT = "utf-8"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("CLIENT_SIDE")
+client.connect((HOST, SERVER_PORT))
+
+print(f"client address: ", client.getsockname())
+
+check_point = True
+while True:
+    message = input("message: ")
+
+    client.sendall(message.encode(FORMAT))
+    check = input("Continue? Y/N")
+    if check in ("Y", "y"):
+        continue
+    else:
+        break
---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = "127.0.0.1"
+SERVER_PORT = 65432
+FORMAT = "utf-8"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, SERVER_PORT))
+s.listen()
+
+print("SERVER SIDE")
+print(f"server: {HOST} {SERVER_PORT}")
+
+conn, addr = s.accept()
+
+print(f"client address: {addr}")
+print("conn: ", conn.getsockname())
+while True:
+    message = conn.recv(1024).decode(FORMAT)
+    print(len(message))
+    if len(message):
+        print(f"message: {message}")
+    else:
+        break
+
+    
